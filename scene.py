f9fb8cfbca6a75fff48422496ec4dfe9f5e8565e
Nick Fisher <github@nickdafish.com>
2015-08-20 21:00:25 -0400

    more formatting fixes

--- scene.py
+++ scene.py
@@ -5,81 +5,81 @@ reload(mazin)
 
 class GridScene(Scene):
 
-	def __init__(self, carver, cols=None, rows=None, cell_size=None):
-		self.cell_size = cell_size
-		self.carver = carver
-		self.cols = cols
-		self.rows = rows
-		super(GridScene, self).__init__()
+    def __init__(self, carver, cols=None, rows=None, cell_size=None):
+        self.cell_size = cell_size
+        self.carver = carver
+        self.cols = cols
+        self.rows = rows
+        super(GridScene, self).__init__()
 
-	def setup(self):
-		if (self.cols or self.rows) and (self.cell_size is None):
-			
-			if self.cols:
-				max_width = int(self.size.w / (self.cols + 1))
-			else:
-				max_width = 99999
+    def setup(self):
+        if (self.cols or self.rows) and (self.cell_size is None):
 
-			if self.rows:
-				max_height = int(self.size.h / (self.rows + 1))
-			else:
-				max_height = 99999
+            if self.cols:
+                max_width = int(self.size.w / (self.cols + 1))
+            else:
+                max_width = 99999
 
-			self.cell_size = max_height if max_height < max_width else max_width
-			
-		if self.cols is self.rows is self.cell_size is None:
-			self.cell_size = 20
+            if self.rows:
+                max_height = int(self.size.h / (self.rows + 1))
+            else:
+                max_height = 99999
 
-		if self.cols is None:
-			max_cols = int(self.size.w / self.cell_size) - 1
-			self.cols = max_cols
+            self.cell_size = max_height if max_height < max_width else max_width
 
-		if self.rows is None:
-			max_rows = int(self.size.h / self.cell_size) - 1
-			self.rows = max_rows
+        if self.cols is self.rows is self.cell_size is None:
+            self.cell_size = 20
 
-		self.hpad = (self.size.w - (self.cols * self.cell_size))/2
-		self.vpad = (self.size.h - (self.rows * self.cell_size))/2
-		self.grid = mazin.Grid(self.cols, self.rows)
+        if self.cols is None:
+            max_cols = int(self.size.w / self.cell_size) - 1
+            self.cols = max_cols
 
-		self.itr = self.carver(self.grid, steps=1)
+        if self.rows is None:
+            max_rows = int(self.size.h / self.cell_size) - 1
+            self.rows = max_rows
 
-		self.delay = 0.00001
-		self.acc = float(self.delay + 1)
+        self.hpad = (self.size.w - (self.cols * self.cell_size))/2
+        self.vpad = (self.size.h - (self.rows * self.cell_size))/2
+        self.grid = mazin.Grid(self.cols, self.rows)
 
-	def draw(self):
+        self.itr = self.carver(self.grid, steps=1)
 
-		self.acc += self.dt
-		if self.acc > self.delay:
-			self.acc = 0
-			try:
-				self.next_cell = self.itr.next()
-			except StopIteration:
-				self.next_cell = None
+        self.delay = 0.00001
+        self.acc = float(self.delay + 1)
 
-		background(255, 255, 255)
-		stroke(0 ,0 ,0)
-		stroke_weight(1)
-		fill(1, 0, 0)
+    def draw(self):
 
-		for cell in self.grid.iter_cells:
-			x1 = self.hpad + (cell.col * self.cell_size)
-			y1 = (self.size.h - (cell.row * self.cell_size)) - self.vpad
-			x2 = self.hpad + ((cell.col + 1) * self.cell_size)
-			y2 = (self.size.h - ((cell.row + 1) * self.cell_size)) - self.vpad
+        self.acc += self.dt
+        if self.acc > self.delay:
+            self.acc = 0
+            try:
+                self.next_cell = self.itr.next()
+            except StopIteration:
+                self.next_cell = None
 
-			if cell is self.next_cell:
-				rect(x1, y2, self.cell_size, self.cell_size)
+        background(255, 255, 255)
+        stroke(0 ,0 ,0)
+        stroke_weight(1)
+        fill(1, 0, 0)
 
-			if cell.north is None:
-				line(x1, y1, x2, y1)
-			if cell.west is None:
-				line(x1, y1, x1, y2)
+        for cell in self.grid.iter_cells:
+            x1 = self.hpad + (cell.col * self.cell_size)
+            y1 = (self.size.h - (cell.row * self.cell_size)) - self.vpad
+            x2 = self.hpad + ((cell.col + 1) * self.cell_size)
+            y2 = (self.size.h - ((cell.row + 1) * self.cell_size)) - self.vpad
 
-			if cell.east not in cell.links:
-				line(x2, y1, x2, y2)
-			if cell.south not in cell.links:
-				line(x1, y2, x2, y2)
+            if cell is self.next_cell:
+                rect(x1, y2, self.cell_size, self.cell_size)
+
+            if cell.north is None:
+                line(x1, y1, x2, y1)
+            if cell.west is None:
+                line(x1, y1, x1, y2)
+
+            if cell.east not in cell.links:
+                line(x2, y1, x2, y2)
+            if cell.south not in cell.links:
+                line(x1, y2, x2, y2)
 
 
 scene = GridScene(mazin.carvers.aldous_broder, 10)
--- tests.py
+++ tests.py
@@ -5,116 +5,116 @@ from mazin.carvers import unroll_steps_zero
 
 
 class GridTest(unittest.TestCase):
-	def test_size(self):
-		grid = Grid(5, 5)
-		self.assertTrue(len(grid) == 5 * 5)
-		self.assertEqual(grid.size, 5 * 5)
+    def test_size(self):
+        grid = Grid(5, 5)
+        self.assertTrue(len(grid) == 5 * 5)
+        self.assertEqual(grid.size, 5 * 5)
 
-	def test_top_left_corner(self):
-		grid = Grid(5, 5)
-		self.assertIsNone(grid[(0, 0)].north)
-		self.assertIsNone(grid[(0, 0)].west)
-		self.assertIsNotNone(grid[(0, 0)].south)
-		self.assertIsNotNone(grid[(0, 0)].east)
+    def test_top_left_corner(self):
+        grid = Grid(5, 5)
+        self.assertIsNone(grid[(0, 0)].north)
+        self.assertIsNone(grid[(0, 0)].west)
+        self.assertIsNotNone(grid[(0, 0)].south)
+        self.assertIsNotNone(grid[(0, 0)].east)
 
-	def test_bottom_right_corner(self):
-		grid = Grid(5, 5)
-		self.assertIsNotNone(grid[(4, 4)].north)
-		self.assertIsNotNone(grid[(4, 4)].west)
-		self.assertIsNone(grid[(4, 4)].south)
-		self.assertIsNone(grid[(4, 4)].east)
+    def test_bottom_right_corner(self):
+        grid = Grid(5, 5)
+        self.assertIsNotNone(grid[(4, 4)].north)
+        self.assertIsNotNone(grid[(4, 4)].west)
+        self.assertIsNone(grid[(4, 4)].south)
+        self.assertIsNone(grid[(4, 4)].east)
 
-	def test_random_cell(self):
-		grid = Grid(5, 5)
-		cell1 = grid.random_cell
-		cell2 = grid.random_cell
-		cell3 = grid.random_cell
-		self.assertIsInstance(cell1, Cell)
-		self.assertIsInstance(cell2, Cell)
-		self.assertIsInstance(cell3, Cell)
+    def test_random_cell(self):
+        grid = Grid(5, 5)
+        cell1 = grid.random_cell
+        cell2 = grid.random_cell
+        cell3 = grid.random_cell
+        self.assertIsInstance(cell1, Cell)
+        self.assertIsInstance(cell2, Cell)
+        self.assertIsInstance(cell3, Cell)
 
 
 class CellTest(unittest.TestCase):
-	def test_cell_list(self):
-		cl = CellList(Cell(0, 0))
-		cl.append(Cell(1, 1))
-		cl.extend([Cell(2, 3)])
-		with self.assertRaises(TypeError):
-			cl.append(3)
-			cl.extend([1, 2, 3])
+    def test_cell_list(self):
+        cl = CellList(Cell(0, 0))
+        cl.append(Cell(1, 1))
+        cl.extend([Cell(2, 3)])
+        with self.assertRaises(TypeError):
+            cl.append(3)
+            cl.extend([1, 2, 3])
 
-	def test_cell_shape(self):
-		cm = Cell(1, 1)
-		ce = Cell(2, 1)
-		cs = Cell(1, 2)
-		cm.east = ce
-		ce.west = cm
-		cm.links += [ce]
-		self.assertEqual(cm.shape, 2)
-		self.assertEqual(ce.shape, 8)
-		cm.south = cs
-		cs.north = cm
-		cm.links += [cs]
-		self.assertEqual(cm.shape, 6)
-		self.assertEqual(cs.shape, 1)
+    def test_cell_shape(self):
+        cm = Cell(1, 1)
+        ce = Cell(2, 1)
+        cs = Cell(1, 2)
+        cm.east = ce
+        ce.west = cm
+        cm.links += [ce]
+        self.assertEqual(cm.shape, 2)
+        self.assertEqual(ce.shape, 8)
+        cm.south = cs
+        cs.north = cm
+        cm.links += [cs]
+        self.assertEqual(cm.shape, 6)
+        self.assertEqual(cs.shape, 1)
 
 
 class TestKeySortingDict(unittest.TestCase):
-	def test_ksd_basic(self):
-		ksd = KeySortingDict()
-		ksd[(2, 2), (1, 1)] = 99
-		self.assertEqual(ksd[(1, 1), (2, 2)], 99)
+    def test_ksd_basic(self):
+        ksd = KeySortingDict()
+        ksd[(2, 2), (1, 1)] = 99
+        self.assertEqual(ksd[(1, 1), (2, 2)], 99)
 
 
 class TestCellSortingDict(unittest.TestCase):
-	def test_csd_basic(self):
-		csd = CellSortingDict()
-		c1 = Cell(1, 1)
-		c2 = Cell(2, 2)
-		csd[c1, c2] = 99
-		self.assertEqual(csd[c1, c2], 99)
-		self.assertEqual(csd[c2, c1], 99)
-		with self.assertRaises(TypeError):
-			csd[(2, 2), (1, 1)] = 99
+    def test_csd_basic(self):
+        csd = CellSortingDict()
+        c1 = Cell(1, 1)
+        c2 = Cell(2, 2)
+        csd[c1, c2] = 99
+        self.assertEqual(csd[c1, c2], 99)
+        self.assertEqual(csd[c2, c1], 99)
+        with self.assertRaises(TypeError):
+            csd[(2, 2), (1, 1)] = 99
 
 
 class TestUnroller(unittest.TestCase):
-	def test_simple(self):
-		@unroll_steps_zero
-		def stepper(steps=0):
-			for x in range(steps):
-				yield 1
+    def test_simple(self):
+        @unroll_steps_zero
+        def stepper(steps=0):
+            for x in range(steps):
+                yield 1
 
-		self.assertIsNone(stepper())
-		self.assertIsNone(stepper(steps=0))
-		self.assertEqual(sum([r for r in stepper(steps=5)]), 5)
+        self.assertIsNone(stepper())
+        self.assertIsNone(stepper(steps=0))
+        self.assertEqual(sum([r for r in stepper(steps=5)]), 5)
 
 
 class TestLinker(unittest.TestCase):
-	def test_basics(self):
-		grid = Grid(3, 3)
-		c = grid[(1, 1)]
-		self.assertFalse(c.links, 'Starting with no links')
-		self.assertFalse(c.link.north, 'No link north')
-		c.link.north = True
-		self.assertTrue(c.north in c.links, 'North should be in links now')
-		self.assertTrue(c.link.north, 'North should be linked')
-		c.link(c.south)
-		self.assertTrue(c.south in c.links, 'South should be in links now')
-		self.assertTrue(c.link.south, 'South should be linked')
+    def test_basics(self):
+        grid = Grid(3, 3)
+        c = grid[(1, 1)]
+        self.assertFalse(c.links, 'Starting with no links')
+        self.assertFalse(c.link.north, 'No link north')
+        c.link.north = True
+        self.assertTrue(c.north in c.links, 'North should be in links now')
+        self.assertTrue(c.link.north, 'North should be linked')
+        c.link(c.south)
+        self.assertTrue(c.south in c.links, 'South should be in links now')
+        self.assertTrue(c.link.south, 'South should be linked')
 
 
 def main():
-	try:
-		import console
-		console.clear()
-	except ImportError:
-		pass
+    try:
+        import console
+        console.clear()
+    except ImportError:
+        pass
 
-	grid = Grid(4, 4)
-	print grid
-	unittest.main()
+    grid = Grid(4, 4)
+    print grid
+    unittest.main()
 
 
 if __name__ == '__main__':
-	main()
+    main()
